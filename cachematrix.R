0a01c7cc4fd5e950ba64673beb50fa04ba568e59
KarneChaithanyaSai <chaithutheprince@gmail.com>
2019-02-11 19:23:26 +0530

    cache matrix and README.md

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This is a R function to create a matrx and cache its inverse.
+## The first R function will create a special "matrix" and this function is able
+## to cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  d <- NULL
+  set <- function(y) {
+    x <<- y
+    d <<- NULL
+  }
+  get <- function() x
+  sinv <- function(SpecialMatrix) d <<- SpecialMatrix
+  ginv <- function() d
+  list(set = set, get = get,
+       sinv = sinv, ginv = ginv)
 }
 
 
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" 
+##  returned by makeCacheMatrix. 
 
 cacheSolve <- function(x, ...) {
+  d <- x$ginv()
+  if(!is.null(d)) {
+    message("getting cached data")
+    return(d)
+  }
+  data <- x$get()
+  d <- solve(data)
+  x$sinv(d)
+  d
         ## Return a matrix that is the inverse of 'x'
 }
